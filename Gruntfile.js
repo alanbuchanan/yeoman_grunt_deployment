0b360ad7c36775cd86fc9569a27a5c006ded8f2d
Rory <rory88@gmail.com>
2015-08-05 02:15:32 +0100

    Gruntfile update

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,8 @@ module.exports = function (grunt) {
 
   var pkg = require('./package.json');
 
+  grunt.loadNpmTasks('grunt-build-control');
+
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
